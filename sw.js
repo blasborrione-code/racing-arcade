c226f31076c56a1fce001dcf4eb16a4933eb9829
blasborrione-code <blasborrione@gmail.com>
2026-02-05 01:11:00 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'racing-game-v2.12'; // Subí a v2.6 para asegurar el cambio
+const CACHE_NAME = 'racing-game-v2.13'; // Subí a v2.6 para asegurar el cambio
 const urlsToCache = [
   'index.html',
   './',
