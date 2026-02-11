cf442bd7b9da6f6ecf1eb29549861d229f672561
blasborrione-code <blasborrione@gmail.com>
2026-02-11 20:42:16 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,7 @@
-const CACHE_NAME = 'racing-game-v4.0'; 
+const CACHE_NAME = 'racing-game-v4.1'; 
 const urlsToCache = [
   'index.html',
   './',
-  // ... (mantené todas tus rutas de autos acá, no las borres)
   'Autos/car_compact_retro.png',
   'Autos/furgoneta.png',
   'Autos/auto_rosa.png',
@@ -12,12 +11,49 @@ const urlsToCache = [
   'Autos/car_luxury_white.png',
   'Autos/car_formula_classic.png',
   'Autos/car_formula_modern.png',
-  'Autos/car_rally_blue_v2.png'
+  'Autos/car_rally_blue_v2.png',
+  'Autos/car_compact_retro_verde.png',
+  'Autos/car_compact_retro_negro.png',
+  'Autos/car_compact_retro_rosa.png',
+  'Autos/car_compact_retro_violeta.png',
+  'Autos/car_compact_retro_azul.png',
+  'Autos/car_compact_retro_blanco.png',
+  'Autos/car_compact_retro_gris.png',
+  'Autos/car_compact_retro_rojo.png',
+  'Autos/car_furgoneta_rojo.png',
+  'Autos/car_furgoneta_verde.png',
+  'Autos/car_furgoneta_amarillo.png',
+  'Autos/car_auto_rosa_negro.png',
+  'Autos/car_auto_rosa_blanco.png',
+  'Autos/car_auto_rosa_verde.png',
+  'Autos/car_auto_rosa_azul.png',
+  'Autos/car_muscle_pony_rosa.png',
+  'Autos/car_muscle_pony_naranja.png',
+  'Autos/car_muscle_pony_celeste.png',
+  'Autos/car_muscle_pony_amarillo.png',
+  'Autos/car_super_yellow_rojo.png',
+  'Autos/car_super_yellow_verde.png',
+  'Autos/car_super_yellow_azul.png',
+  'Autos/car_super_yellow_rosa.png',
+  'Autos/car_super_yellow_negro.png',
+  'Autos/car_sport_black_plata.png',
+  'Autos/car_sport_black_rojo.png',
+  'Autos/car_sport_black_azul.png',
+  'Autos/car_sport_black_verde.png',
+  'Autos/car_sport_black_amarillo.png',
+  'Autos/car_luxury_white_negro.png',
+  'Autos/car_luxury_white_naranja.png',
+  'Autos/car_formula_classic_azul.png',
+  'Autos/car_formula_classic_amarillo.png',
+  'Autos/car_formula_modern_violeta.png',
+  'Autos/car_formula_modern_azul.png',
+  'Autos/car_formula_modern_rojo.png',
+  'Autos/car_formula_modern_verde.png'
 ];
 
 // 1. INSTALACIÓN
 self.addEventListener('install', event => {
-  self.skipWaiting();
+  self.skipWaiting(); 
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
@@ -36,11 +72,11 @@ self.addEventListener('activate', event => {
   );
 });
 
-// 3. FETCH (EL ARREGLO ESTÁ ACÁ)
+// 3. FETCH (EL ARREGLO PARA EL LOGIN)
 self.addEventListener('fetch', event => {
-  // REGLA DE ORO: Si es login de Google o Firebase, NO USAR CACHE
+  // Ignoramos peticiones de Auth para que Google maneje la sesión libremente
   if (event.request.url.includes('/__/auth/') || event.request.url.includes('google.com')) {
-    return; // El SW no se mete en estas peticiones
+    return; 
   }
 
   event.respondWith(
